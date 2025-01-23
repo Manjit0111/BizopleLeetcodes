d08df529ea8481ba77bc0ba3090d69b86556310f
Manjit0111 <manjitbizople@gmail.com>
2025-01-23 10:07:24 +0530

    Delete sortedArray.py

--- sortedArray.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-    def search(self, nums: List[int], target: int) -> int:
-        for index in range(len(nums)):
-            if nums[index]==target:
-                return index
-        return -1
